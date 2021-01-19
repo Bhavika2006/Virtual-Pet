9ba557247f96f3466692739cec59bc70c22b8952
Bhavika2006 <70019267+Bhavika2006@users.noreply.github.com>
2021-01-19 20:05:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,14 +2,21 @@ var  dog, happyDog, database, foodS, foodStock,dogImg
 var database;
 function preload()
 {
-dogImg = loadImage("images/dogImg.png")
-dogHappy = loadImage("images/dogImg1.png")
+dogImg = loadImage("images/Dog.png")
+dogHappy = loadImage("images/happydog.png")
 }
 
 function setup() {
   createCanvas(500, 500);
   database = firebase.database();
  
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  foodStock = database.ref("food") 
+ foodStock.on("value",readStock)
+
 }
 
 
@@ -21,18 +28,30 @@ if(keyWentDown(UP_ARROW)){
   dog.addImage(dogHappy)
 }
 
- foodStock = database.ref("food") 
- goodStock.on("value",function(data){
-   FoodS = data.val()
- })
-
- text("PRESS UP ARROW KEY TO FEED THE DOG")
+fill(255,255,254);
+stroke("black");
+textSize(13);
+text("Food remaining : "+foodS,170,200);
+ text("PRESS UP ARROW KEY TO FEED THE DOG",130,10,300,20)
  
   drawSprites();
- 
-  
-
 }
 
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    food:x
+  })
+}
 
 
